0b5d2e380d734ed3104b84e62d8a858761a9ab12
Konrad <konrad@pilot-in.com>
2018-05-04 06:08:40 +0200

    Fix team meta

--- sections/cards-team.php
+++ sections/cards-team.php
@@ -17,17 +17,17 @@ if($section['result']->have_posts()): ?>
 
                 <h4 class="name"><?php the_title(); ?></h4>
                 
-                <?php if($job_title = $post->wpsst_team_meta['job_title']){ ?>
-                    <span class="job-title"><?php echo $job_title; ?></span>
+                <?php if(isset($post->wpsst_team_meta['job_title'])){ ?>
+                    <span class="job-title"><?php echo $post->wpsst_team_meta['job_title']; ?></span>
                 <?php } ?>
 
                 <?php the_excerpt(); ?>
                 
             </div><!-- end .content -->
             
-            <?php if($social = $post->wpsst_team_meta['social']){ ?>
+            <?php if(isset($post->wpsst_team_meta['social'])){ ?>
                 <ul class="social-links">
-                    <?php foreach($social as $meta){ ?>
+                    <?php foreach($post->wpsst_team_meta['social'] as $meta){ ?>
                         <li class="<?php echo $meta['slug']; ?>"><a href="<?php echo $meta['link']; ?>"><?php echo $meta['title']; ?></a></li>
                     <?php } ?>
                 </ul><!-- end .social-links -->
